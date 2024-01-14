ac7e38ba78414904427441001c87bda512ae4438
Vũ Xuân Mạnh <51844022+vxmanh26@users.noreply.github.com>
2024-01-14 22:06:46 +0700

    Update module.ts

--- src/module.ts
+++ src/module.ts
@@ -1,31 +1,31 @@
-// import { defineNuxtModule, addPlugin } from '@nuxt/kit'
-// import { fileURLToPath } from 'url'
-// import { defu } from 'defu'
-// import { resolve } from 'path'
-// export interface ModuleOptions {
-//   enabled: boolean
-// }
+import { defineNuxtModule, addPlugin } from '@nuxt/kit'
+import { fileURLToPath } from 'url'
+import { defu } from 'defu'
+import { resolve } from 'path'
+export interface ModuleOptions {
+  enabled: boolean
+}
 
-// export default defineNuxtModule<ModuleOptions>({
-//   meta: {
-//     name: 'device-info',
-//     configKey: 'deviceInfo'
-//   },
-//   // Default configuration options of the Nuxt module
-//   defaults: {
-//     enabled: true,
-//   },
-//   setup (options, nuxt) {
-//     if (options.enabled) {
-//       nuxt.options.runtimeConfig.public.device = defu(nuxt.options.runtimeConfig.public.device, {
-//         enabled: options.enabled
-//       })
-//       const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
-//       nuxt.options.build.transpile.push(runtimeDir)
-//       addPlugin(resolve(runtimeDir, 'plugin'))
-//       nuxt.hook('imports:dirs', (dirs) => {
-//         dirs.push(resolve(runtimeDir, 'composables'))
-//       })
-//     }
-//   }
-// })
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'device-info',
+    configKey: 'deviceInfo'
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {
+    enabled: true,
+  },
+  setup (options, nuxt) {
+    if (options.enabled) {
+      nuxt.options.runtimeConfig.public.device = defu(nuxt.options.runtimeConfig.public.device, {
+        enabled: options.enabled
+      })
+      const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
+      nuxt.options.build.transpile.push(runtimeDir)
+      addPlugin(resolve(runtimeDir, 'plugin'))
+      nuxt.hook('imports:dirs', (dirs) => {
+        dirs.push(resolve(runtimeDir, 'composables'))
+      })
+    }
+  }
+})
